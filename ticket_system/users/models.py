4a25777e8a24dd1d7f559c3b99d403b79e980bc7
IMperiumX <yusufadell.dev@gmail.com>
2025-03-13 02:26:23 +0200

    Add Agent model to associate with User for agent-specific functionality

--- ticket_system/users/models.py
+++ ticket_system/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -24,3 +25,10 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
