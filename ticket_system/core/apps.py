ea2195f9040a026493499fbb6558f66b575f2ac9
IMperiumX <yusufadell.dev@gmail.com>
2025-03-13 02:25:30 +0200

    Refactor Core app configuration to update name and import signals on ready

--- ticket_system/core/apps.py
+++ ticket_system/core/apps.py
@@ -1,6 +1,13 @@
+import contextlib
+
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "core"
+    name = "ticket_system.core"
+
+    def ready(self):
+        with contextlib.suppress(ImportError):
+            import ticket_system.core.signals  # noqa: F401
