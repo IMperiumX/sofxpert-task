23b633d58736ba0976500a3f648083756cb280bc
IMperiumX <yusufadell.dev@gmail.com>
2025-03-13 02:26:19 +0200

    Add Ticket and Agent admin interfaces with inline ticket management

--- ticket_system/users/admin.py
+++ ticket_system/users/admin.py
@@ -4,8 +4,10 @@ from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.utils.translation import gettext_lazy as _
 
+from ticket_system.core.models import Ticket
+
 from .forms import UserAdminChangeForm
-from .forms import UserAdminCreationForm
+from .models import Agent
 from .models import User
 
 if settings.DJANGO_ADMIN_FORCE_ALLAUTH:
@@ -18,7 +20,6 @@ if settings.DJANGO_ADMIN_FORCE_ALLAUTH:
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
     form = UserAdminChangeForm
-    add_form = UserAdminCreationForm
     fieldsets = (
         (None, {"fields": ("username", "password")}),
         (_("Personal info"), {"fields": ("name", "email")}),
@@ -38,3 +39,24 @@ class UserAdmin(auth_admin.UserAdmin):
     )
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 0
+    fields = (
+        "title",
+        "subject",
+        "description",
+        "status",
+        "assigned_agent",
+        "created_at",
+    )
+    readonly_fields = ("created_at",)
+
+
+@admin.register(Agent)
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ("id", "user")
+    list_filter = ("user",)
+    inlines = [TicketInline]
