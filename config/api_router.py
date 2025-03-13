26fc938875be0d689d72127248728374bfaa3c60
IMperiumX <yusufadell.dev@gmail.com>
2025-03-13 02:25:18 +0200

    Add Ticket and Agent viewsets to API router

--- config/api_router.py
+++ config/api_router.py
@@ -2,11 +2,15 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
+from ticket_system.core.api.views import AgentViewSet
+from ticket_system.core.api.views import TicketViewSet
 from ticket_system.users.api.views import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("tickets", TicketViewSet)
+router.register("agents", AgentViewSet)
 
 
 app_name = "api"
